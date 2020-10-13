422c62520544b7a086da291980ee29ba47345078
thearjun <be2017se573@gces.edu.np>
2020-10-13 20:31:07 +0545

    Update admin panel

--- cafeteria/admin.py
+++ cafeteria/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django.db.models import Sum
+from django.db.models.signals import post_delete
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+from .models import DailyBalance
 from .models import Expense
 from .models import Income
 from .models import Particular
+from .models import Transaction
 
 
 @admin.register(Particular)
@@ -54,6 +60,8 @@ class IncomeAdmin(admin.ModelAdmin):
     def selling_unit_price(self, obj):
         return obj.particular.selling_unit_price
 
+    date_hierarchy = 'date'
+
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
@@ -88,3 +96,91 @@ class ExpenseAdmin(admin.ModelAdmin):
 
     def cost_unit_price(self, obj):
         return obj.particular.cost_unit_price
+
+    date_hierarchy = 'date'
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = [
+        'date',
+        'party',
+        'amount',
+        'is_expenditure',
+        'remarks',
+    ]
+
+    def remarks(self, obj):
+        return obj.reason
+
+    list_filter = [
+        'date',
+        'party',
+        'is_expenditure',
+    ]
+
+    date_hierarchy = 'date'
+
+
+@admin.register(DailyBalance)
+class DailyBalanceAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'date',
+        'opening_balance',
+        'income',
+        'expense',
+        'incoming_balance',
+        'outgoing_balance',
+        'closing_balance',
+    ]
+
+    list_filter = [
+        'date',
+    ]
+
+    date_hierarchy = 'date'
+
+    def income(self, obj):
+        return Income.objects.filter(date=obj.date).aggregate(
+            total=Sum('net_total')).get('total', 0.0)
+
+    def expense(self, obj):
+        return Expense.objects.filter(date=obj.date).aggregate(
+            total=Sum('total_price')).get('total', 0.0)
+
+    def incoming_balance(self, obj):
+        return Transaction.objects.filter(
+            date=obj.date, is_expenditure=False).aggregate(total=Sum('amount')).get('total', 0.0)
+
+    def outgoing_balance(self, obj):
+        return Transaction.objects.filter(
+            date=obj.date, is_expenditure=True).aggregate(total=Sum('amount')).get('total', 0.0)
+
+
+@receiver(post_save, sender=Income)
+@receiver(post_save, sender=Expense)
+@receiver(post_save, sender=Transaction)
+def update_daily_balance_after_save(sender, instance, created, **kwargs):
+
+    try:
+        daily_balance = DailyBalance.objects.get(date=instance.date)
+        daily_balance.save()
+    except DailyBalance.DoesNotExist:
+        # This will automatically call save() method.
+        DailyBalance.objects.create(date=instance.date, opening_balance=0)
+
+
+@receiver(post_delete, sender=Income)
+@receiver(post_delete, sender=Expense)
+@receiver(post_delete, sender=Transaction)
+def update_daily_balance_after_delete(sender, instance, **kwargs):
+
+    try:
+        daily_balance = DailyBalance.objects.get(date=instance.date)
+        if Income.objects.filter(date=instance.date).count() == 0 and Expense.objects.filter(date=instance.date).count() == 0 and Transaction.objects.filter(date=instance.date).count() == 0:
+            daily_balance.delete()
+        else:
+            daily_balance.save()
+    except DailyBalance.DoesNotExist:
+        pass
--- cafeteria/models.py
+++ cafeteria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Sum
 from django.utils.translation import gettext_lazy as _
 from utils.fields import PercentField
 from utils.functions import calculate_percentage
@@ -90,3 +91,59 @@ class Expense(TimeStampedModelMixin):
         self.total_price = self.quantity * cost_unit_price
 
         super(Expense, self).save(*args, **kwargs)
+
+
+class Transaction(TimeStampedModelMixin):
+
+    date = models.DateField()
+    party = models.CharField(max_length=255)
+    reason = models.CharField(max_length=255, null=True, blank=True)
+    amount = models.FloatField()
+    is_expenditure = models.BooleanField(
+        _('Is Expenditure or not ?'),
+        help_text=_(
+            'Click on checkbox for general expenditure and don\'t click for investment'),
+        default=False
+    )
+
+    def __str__(self):
+        return f'{self.date} - {self.party} - {self.amount}'
+
+
+class DailyBalance(TimeStampedModelMixin):
+
+    date = models.DateField(unique=True)
+    opening_balance = models.FloatField(_('Opening Balance'))
+    closing_balance = models.FloatField(_('Closing Balance'))
+
+    def __str__(self):
+        return f'{self.date}'
+
+    def save(self, *args, **kwargs):
+
+        date = self.date
+        opening_balance = self.opening_balance
+
+        total_income = Income.objects.filter(date=date).aggregate(
+            total=Sum('net_total')).get('total', 0.0)
+        total_expense = Expense.objects.filter(date=date).aggregate(
+            total=Sum('total_price')).get('total', 0.0)
+        investment = Transaction.objects.filter(
+            date=date, is_expenditure=False).aggregate(total=Sum('amount')).get('total', 0.0)
+        expenditure = Transaction.objects.filter(
+            date=date, is_expenditure=True).aggregate(total=Sum('amount')).get('total', 0.0)
+
+        if total_income is None:
+            total_income = 0
+        if total_expense is None:
+            total_expense = 0
+        if investment is None:
+            investment = 0
+        if expenditure is None:
+            expenditure = 0
+
+        print(total_income, total_expense, investment, expenditure)
+
+        self.closing_balance = opening_balance + total_income + \
+            investment - total_expense - expenditure
+        super(DailyBalance, self).save(*args, **kwargs)

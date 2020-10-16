466b67add9d9ac263d55372b0d22c73d31d2b30f
thearjun <be2017se573@gces.edu.np>
2020-10-16 17:27:05 +0545

    Update STATIC  settings

--- gyanhub/settings.py
+++ gyanhub/settings.py
@@ -129,8 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static_files'
 STATICFILES_DIRS = [
-    (BASE_DIR / 'static'),
+    BASE_DIR / 'static',
 ]
 
 

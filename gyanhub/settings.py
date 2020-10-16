5d99bde14cbde38d3a682261f68af4098e07d437
thearjun <be2017se573@gces.edu.np>
2020-10-16 16:51:36 +0545

    Update requirements and ALLOWED_HOSTS

--- gyanhub/settings.py
+++ gyanhub/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^b3*$q6g^k938tsr^fl4&xgs!*v7!z5*rxsc3k=vkstw9y@=@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

c18e7b322d12f3bb27cb4bb03258f05a4d9ca24b
thearjun <be2017se573@gces.edu.np>
2020-10-16 17:51:51 +0545

    Update mod_wsgi conf

--- gyanhub/settings.py
+++ gyanhub/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static_files'
+STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 

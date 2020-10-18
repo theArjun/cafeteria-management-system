5832f78d2ccc05bf734ce3249d88f6dad580e461
thearjun <be2017se573@gces.edu.np>
2020-10-18 09:11:13 +0545

    Update static setting

--- gyanhub/settings.py
+++ gyanhub/settings.py
@@ -132,10 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static_files/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_DIRS = ( BASE_DIR / 'static/', )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
